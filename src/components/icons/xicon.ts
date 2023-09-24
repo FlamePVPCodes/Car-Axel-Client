bffdd254fa370f6364cec553a7ef11084d117831
FlamePVPCodes <142227927+FlamePVPCodes@users.noreply.github.com>
2023-09-24 03:22:11 -0400

    Update xicon.ts

--- src/components/icons/xicon.ts
+++ src/components/icons/xicon.ts
@@ -1,40 +1,43 @@
-import { Icon, createElement } from "../../UILib"
+import { Icon, createElement } from "../../UILib";
+import "./icon.ts.less";
 
-import "./icon.ts.less"
-
-// i bet some random guy looked at this thinking its good code.
-// (its not, its absolute trash, and a pain to refractor)
-export default class Destroy implements Icon {   
+export default class XIcon implements Icon {
     public parent: HTMLElement;
     public icon: string;
     public name: string;
-    public hoverName!: HTMLElement; 
+    public hoverName!: HTMLElement;
     private iconElement!: HTMLElement;
 
     constructor(parent: HTMLElement, name: string, icon: string) {
         this.parent = parent;
         this.icon = icon;
-        this.name = name; 
-
-        this.render()
+        this.name = name;
+        this.render();
     }
 
-    action(): void {
-        document.getElementById("cac__CONTAINER")?.remove() 
-    }
+    action = (): void => {
+        // Define the action to be performed when the icon is clicked.
+        // Example: closing a window, removing an element, etc.
+        const container = document.getElementById("cac__CONTAINER");
+        if (container) {
+            container.style.animation = "fadeout 0.3s ease-in-out";
+            setTimeout(() => {
+                container.remove();
+            }, 300);
+        }
+    };
 
     render(): void {
         this.iconElement = createElement("button", this.parent, {
             className: "cac__tabbar__button",
-            innerHTML: this.icon
-        })
+            innerHTML: this.icon,
+        });
 
         this.iconElement.onclick = this.action;
-        
+
         this.hoverName = createElement("p", this.iconElement, {
             className: "cac__tabbar__button__hovername",
-            innerHTML: this.name
-        })
-        
+            innerHTML: this.name,
+        });
     }
 }
