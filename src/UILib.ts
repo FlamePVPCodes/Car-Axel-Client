6243abe7ad3ef629d9102191f87bb57775458b17
FlamePVPCodes <142227927+FlamePVPCodes@users.noreply.github.com>
2023-09-24 03:23:23 -0400

    Update UILib.ts

--- src/UILib.ts
+++ src/UILib.ts
@@ -1,56 +1,45 @@
-
 export interface Component {
   parent: HTMLElement;
   render(): void;
 }
 
 export interface Icon extends Component {
-    icon: string; //SVG Element
-    action(...args:Array<any>): void;
+  icon: string; // SVG Element
+  action(...args: any[]): void;
 }
 
 interface ElementAttributes {
-    id?: string;
-    className?: string;
-    innerHTML?: string
-    type?: string;
-    name?: string;
-    value?: string;
+  id?: string;
+  className?: string;
+  innerHTML?: string;
+  type?: string;
+  name?: string;
+  value?: string;
 }
 
 export function createElement(
-    tagName: string,
-    parent: Element,
-    options?: ElementAttributes,
-    ): HTMLElement {
-    const element: HTMLElement = document.createElement(tagName);
-    // TODO:: Make this better or use a switch or smth
-    if (options) {
-        if (options.id) {
-            element.id = options.id;
-        }
-        if (options.className) {
-            element.className = options.className;
-        }
-        if (options.innerHTML) {
-            element.innerHTML = options.innerHTML;
-        }
-        if (options.type) {
-            element.setAttribute("type", options.type);
-        }
-        if (options.name) {
-             element.setAttribute("name", options.name);
-        }
-        if (options.value) {
-            element.setAttribute("value", options.value);
-        }
-    }
+  tagName: string,
+  parent: Element,
+  options?: ElementAttributes
+): HTMLElement {
+  const element: HTMLElement = document.createElement(tagName);
 
-    element.classList.add("cac__ALL")
+  if (options) {
+    const { id, className, innerHTML, type, name, value } = options;
 
-    if (parent) {
-        parent.appendChild(element);
-    }
+    if (id) element.id = id;
+    if (className) element.className = className;
+    if (innerHTML) element.innerHTML = innerHTML;
+    if (type) element.setAttribute("type", type);
+    if (name) element.setAttribute("name", name);
+    if (value) element.setAttribute("value", value);
+  }
 
-    return element;
+  element.classList.add("cac__ALL");
+
+  if (parent) {
+    parent.appendChild(element);
+  }
+
+  return element;
 }
