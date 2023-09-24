e8421301c3aa50c3704f1d6e055aa4eb37aea748
FlamePVPCodes <142227927+FlamePVPCodes@users.noreply.github.com>
2023-09-24 03:20:51 -0400

    Update saveicon.ts

--- src/components/icons/saveicon.ts
+++ src/components/icons/saveicon.ts
@@ -1,52 +1,46 @@
-import { Icon, createElement } from "../../UILib"
+import { Icon, createElement } from "../../UILib";
 import { UIManager } from "../../UIManager";
-import "./icon.ts.less"
+import "./icon.ts.less";
 
-
-export default class AddSave implements Icon {   
+export default class AddSave implements Icon {
     public parent: HTMLElement;
     public icon: string;
     public name: string;
-    public hoverName!: HTMLElement; 
+    public hoverName!: HTMLElement;
     private iconElement!: HTMLElement;
 
     constructor(parent: HTMLElement, name: string, icon: string, UI: UIManager) {
         this.parent = parent;
         this.icon = icon;
         this.name = name;
-        this.render()
-        this.iconElement.onclick = () => { this.action(UI) }
+        this.render();
+        this.iconElement.onclick = () => this.action(UI);
     }
 
     action(UI: UIManager): void {
-        console.log("%cAttempting Save", "color:blue; font-size: 3vw;")
+        console.log("%cAttempting Save", "color: blue; font-size: 3vw;");
 
-        // too lazy to rigidly define values
-        let cacStorage: Array<any> = [];
-        
-        for (let section of UI.sections) {
-            let buttonValues = section.getAllButtonValues()
-            console.log("%c" + section.id, "font-size:1.25vw;")
-            console.table(buttonValues)
-            cacStorage.push({"sectionID": section.id, buttonValues})
+        const cacStorage: { sectionID: string; buttonValues: boolean[][] }[] = [];
+
+        for (const section of UI.sections) {
+            const buttonValues = section.getAllButtonValues();
+            console.log("%c" + section.id, "font-size: 1.25vw;");
+            console.table(buttonValues);
+            cacStorage.push({ sectionID: section.id, buttonValues });
         }
 
-
-        localStorage.setItem("car-axle-client", JSON.stringify(cacStorage))
-
-
+        localStorage.setItem("car-axle-client", JSON.stringify(cacStorage));
     }
 
     render(): void {
         this.iconElement = createElement("button", this.parent, {
             className: "cac__tabbar__button",
-            innerHTML: this.icon
-        })
+            innerHTML: this.icon,
+        });
 
         this.hoverName = createElement("p", this.iconElement, {
             className: "cac__tabbar__button__hovername",
-            innerHTML: this.name
-        })
-        
+            innerHTML: this.name,
+        });
     }
 }
