cfc741b6b0a178e6b5b1265d66c7944e9b2fa064
FlamePVPCodes <142227927+FlamePVPCodes@users.noreply.github.com>
2023-09-24 03:26:28 -0400

    Update updater.ts

--- src/updater.ts
+++ src/updater.ts
@@ -1,22 +1,33 @@
-// Custom UPDATER for car-axle-client
-import Notification from "./components/notification"
+import Notification from "./components/notification";
 
-const current_version = 1
-const i = 0
+// Define the current version and update index
+const currentVersion = 2;
 
-function showUpdate(parent: HTMLElement, new_ver: string) {
-    let notifcation = new Notification(parent, "A New Update is Available!", `You can update from <strong>v${current_version} (i: ${i}) -> v${new_ver}</strong><br> Updating can fix issues you may currently be having and will add new features. They can also cause issues. Be careful!`, {text: "Install", fn: () => window.open("https://car-axle-client.github.io/install")})
+function showUpdate(parent: HTMLElement, newVersion: string) {
+  const notification = new Notification(
+    parent,
+    "A New Update is Available!",
+    `You can update from <strong>v${currentVersion} -> v${newVersion}</strong><br> Updating can fix issues you may currently be having and will add new features. They can also cause issues. Be careful!`,
+    { text: "Install", fn: () => window.open("https://car-axle-client.github.io/install") }
+  );
 }
 
+// Function to check for updates
 export function getUpdate(main: HTMLElement) {
-
-    fetch("https://raw.githubusercontent.com/car-axle-client/car-axle-database/main/version.json")
-        .then(result => result.json())
-        .then(json => {
-            if (current_version >= Number(json["version"])) {
-                console.log("Version is UP TO DATE")
-            } else {
-                showUpdate(main, `${json["version"]} (i: ${json["i"]})`)
-            }
-        })
+  fetch("https://raw.githubusercontent.com/car-axle-client/car-axle-database/main/version.json")
+    .then((result) => {
+      if (!result.ok) {
+        throw new Error(`Network response was not ok: ${result.status} ${result.statusText}`);
+      }
+      return result.json();
+    })
+    .then((json) => {
+      const newVersion = `${json["version"]}`;
+      if (currentVersion < Number(json["version"])) {
+        showUpdate(main, newVersion);
+      }
+    })
+    .catch((error) => {
+      console.error("Error checking for updates:", error);
+    });
 }
