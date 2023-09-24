4d11c7b90cb44496e423cdf1cb9f774f2535b394
FlamePVPCodes <142227927+FlamePVPCodes@users.noreply.github.com>
2023-09-24 03:06:52 -0400

    Update notification.ts

--- src/components/notification.ts
+++ src/components/notification.ts
@@ -1,40 +1,52 @@
-import "./notification.ts.less"
-import { Component, createElement } from "../UILib"
-
-// yahhhh
-//
-// 
-//
+import "./notification.ts.less";
+import { Component, createElement } from "../UILib";
 
 export default class Notification implements Component {
   parent: HTMLElement;
-  notification!: HTMLElement;
+  notification: HTMLElement | null = null;
   title: string;
   body: string;
 
-  constructor(parent: HTMLElement, title: string, body: string, button?: {text: string, fn: () => void}) {
-    this.parent = parent
-    this.title = title
-    this.body = body
+  constructor(parent: HTMLElement, title: string, body: string, button?: { text: string; fn: () => void }) {
+    this.parent = parent;
+    this.title = title;
+    this.body = body;
 
-    this.render()
-    // custom stuff
+    this.render();
+
     if (button) {
-      createElement("button", this.notification, {className: "cac__notification__button", innerHTML: button.text})
-        .onclick = e => button.fn()
+      const buttonElement = createElement("button", this.notification!, {
+        className: "cac__notification__button",
+        innerHTML: button.text,
+      });
+      buttonElement.onclick = () => button.fn();
     }
   }
-  
+
   render() {
-    this.notification = createElement("div", this.parent, {className: "cac__notification"})
+    this.notification = createElement("div", this.parent, { className: "cac__notification" });
 
-    createElement("h1", this.notification, {className: "cac__notification__header", innerHTML: this.title})
-    createElement("p", this.notification, {className: "cac__notification__body", innerHTML: this.body})
-    createElement("button", this.notification, {className: "cac__notification__button", innerHTML: "Dismiss"})
-      .onclick = e => this.notification.remove()
-  } 
+    createElement("h1", this.notification, { className: "cac__notification__header", innerHTML: this.title });
+    createElement("p", this.notification, { className: "cac__notification__body", innerHTML: this.body });
 
-  display(ms: Number = 500) {
-    //todo
+    const dismissButton = createElement("button", this.notification, {
+      className: "cac__notification__button",
+      innerHTML: "Dismiss",
+    });
+    dismissButton.onclick = () => this.dismiss();
   }
-} 
+
+  display(ms: number = 500) {
+    if (this.notification) {
+      setTimeout(() => {
+        this.dismiss();
+      }, ms);
+    }
+  }
+
+  private dismiss() {
+    if (this.notification) {
+      this.notification.remove();
+    }
+  }
+}
